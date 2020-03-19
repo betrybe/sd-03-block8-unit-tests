6d889494236f6d4a1fe7d69bb1bb637f0e099682
Mauricio Homma <mhhomma@gmail.com>
2020-03-19 16:33:04 -0300

    fix circle jest

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const circle = require('../src/circle');
 
-describe(#circle, () => {
+describe('#circle', () => {
   it('given a radius, should return an object with circles info' () => {
     
     // ESCREVA SEUS TESTES AQUI
