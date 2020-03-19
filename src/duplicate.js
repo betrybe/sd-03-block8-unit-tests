ca0e7857a07e49c879f4f9b7f587f835a3853bb2
Mauricio Homma <mhhomma@gmail.com>
2020-03-19 18:49:42 -0300

    Remove spread operator challenges

--- src/duplicate.js
+++
@@ -1,8 +0,0 @@
-/*
-  A função duplicate recebe uma lista de parâmetros (tamanho variável)
-  e retorna `true` se existe algum elemento duplicado e `false` caso contrário
- */
-
-const duplicate = () => {}
-
-module.exports = duplicate;
