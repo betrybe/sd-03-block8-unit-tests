677ff38d3f1ed9cdb59d6e8537843ec6408e1fc9
Mauricio Homma <mhhomma@gmail.com>
2020-03-20 17:03:41 -0300

    Remove duplicate requisite

--- src/duplicate.js
+++
@@ -1,8 +0,0 @@
-/*
-  A função duplicate recebe um array (tamanho variável)
-  e retorna `true` se existe algum elemento duplicado e `false` caso contrário
- */
-
-const duplicate = () => {}
-
-module.exports = duplicate;
