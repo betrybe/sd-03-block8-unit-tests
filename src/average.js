e069f31a50bccdd3a590c6b8fbfd986605ae8a2e
Mauricio Homma <mhhomma@gmail.com>
2020-03-20 09:48:15 -0300

    Revert "Remove spread operator challenges"
    This reverts commit ca0e7857a07e49c879f4f9b7f587f835a3853bb2.

---
+++ src/average.js
@@ -0,0 +1,10 @@
+/*
+  A função average recebe uma lista de parâmetros (tamanho variável)
+  e retorna a média dos valores recebidos
+  Caso a função receba algum valor não número, o valor undefined deve ser retornado
+*/
+
+const average = (...params) => {
+}
+
+module.exports = average;
