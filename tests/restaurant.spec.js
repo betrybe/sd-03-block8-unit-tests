fff9ae3243bde4809b2c8aeed7ccde11c4192215
Jean Vasconcelos <jean.vasconcelos@betrybe.com>
2020-03-23 15:24:07 -0300

    add test

---
+++ tests/restaurant.spec.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('#createMenu', () => {
+  it('given a radius, should return an object with circles info', () => {
+
+    assert.equal(true, true);
+
+  });
+});
